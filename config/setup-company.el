f36ad9fd9aa693cd06728e1602cdd570b2189402
hackartist <hackartist.anon@gmail.com>
2016-03-24 12:07:03 +0900

    some changed
    Signed-off-by: hackartist <hackartist.anon@gmail.com>

--- config/setup-company.el
+++ config/setup-company.el
@@ -4,7 +4,7 @@
 (delete 'company-semantic company-backends)
 (define-key c-mode-map  [(tab)] 'company-complete)
 (define-key c++-mode-map  [(tab)] 'company-complete)
-
+;;(define-key python-mode-map [(tab)] 'company-complete)
 ;; (add-to-list 'company-backends 'company-c-headers)
 ;; (add-to-list 'company-backends 'company-jedi)
 
