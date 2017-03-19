373b5c9e6a4c680f01be055345b13ee317025683
Jongseok Choi <hackartist.anon@gmail.com>
2017-03-19 09:35:55 +0900

    flyspell-mode in LaTex-mode was enabled

--- config/setup-tex.el
+++ config/setup-tex.el
@@ -1,13 +1,13 @@
-;;(setq TeX-auto-save t)
-;;(setq TeX-parse-self t)
-;;(setq TeX-save-query nil)
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq TeX-save-query nil)
 ;(setq TeX-PDF-mode t)
 
-;; (setq ispell-program-name "aspell") ; could be ispell as well, depending on your preferences
-;; (setq ispell-dictionary "english") ; this can obviously be set to any language your spell-checking program supports
+(setq ispell-program-name "aspell") ; could be ispell as well, depending on your preferences
+(setq ispell-dictionary "english") ; this can obviously be set to any language your spell-checking program supports
 
-;; (add-hook 'LaTeX-mode-hook 'flyspell-mode)
-;; (add-hook 'LaTeX-mode-hook 'flyspell-buffer)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-buffer)
 
 (defun turn-on-outline-minor-mode ()
   (outline-minor-mode 1))
