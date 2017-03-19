9e5b880251094e298e842b965ddfc20efdd6e1b4
Jongseok Choi <hackartist.anon@gmail.com>
2017-03-19 14:14:50 +0900

    auxtex setting

--- config/setup-tex.el
+++ config/setup-tex.el
@@ -1,7 +1,7 @@
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
 (setq TeX-save-query nil)
-                                        ;(setq TeX-PDF-mode t)
+(setq TeX-PDF-mode t)
 
 (setq ispell-program-name "aspell") ; could be ispell as well, depending on your preferences
 (setq ispell-dictionary "english") ; this can obviously be set to any language your spell-checking program supports
@@ -22,14 +22,9 @@
             )
           )
 
-;; (add-to-list 'TeX-command-list '(
-;;                                  "pdflatex \\nonstopmode\\input %s.tex"
-;;                                  "pdflatex \\nonstopmode\\input %s.tex"
-;;                                  TeX-run-command
-;;                                  nil
-;;                                  t
-;;                                  :help "pdflatex \\nonstopmode\\input golang.tex"
-;;                                  ))
+(eval-after-load "tex"
+  '(add-to-list 'TeX-command-list
+		'("Pdflatex" "pdflatex %s" TeX-run-command nil t :help "Run pdflatex") t))
 
 
 (provide 'setup-tex)
