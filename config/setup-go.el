09c2f1338df8ffdbe51bc7ecb5681ca819cff352
Jongseok Choi <jseokchoi@smilegate.com>
2017-03-28 13:51:17 +0900

    company-go was applied

--- config/setup-go.el
+++ config/setup-go.el
@@ -1,18 +1,19 @@
 (add-to-list 'load-path "~/go/src/github.com/dougm/goflymake")
 (require 'go-flycheck)
-(require 'go-autocomplete)
-(require 'auto-complete-config)
+;;(require 'go-autocomplete)
+;;(require 'auto-complete-config)
 
 
 (defun my-go-mode-hook()
   (add-hook 'before-save-hook 'gofmt-before-save)
-  (ac-config-default)
+  ;;(ac-config-default)
   (setq gofmt-command "goimports")
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
                       "go build -v && go test -v && go vet"))
   (local-set-key (kbd "M-.") 'godef-jump)
   (local-set-key (kbd "M-*") 'pop-tag-mark)
+  (add-to-list 'company-backends 'company-go)
   )
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
--- init.el
+++ init.el
@@ -97,7 +97,7 @@
  '(ecb-options-version "2.40")
  '(package-selected-packages
    (quote
-    (flymake flymake-yaml yaml-mode company-auctex auctex find-temp-file company-distel zygospore xcscope ws-butler volatile-highlights undo-tree tabbar-ruler sws-mode sr-speedbar smartparens py-autopep8 magit jedi jdee jade-mode iedit highlight-current-line helm-swoop helm-projectile helm-gtags go-guru go-errcheck go-autocomplete ggtags function-args flycheck-tip exec-path-from-shell emacs-eclim elpy ein ecb duplicate-thing dtrt-indent company-jedi company-c-headers comment-dwim-2 clean-aindent-mode auto-complete-distel anzu))))
+    (company-go flymake flymake-yaml yaml-mode company-auctex auctex find-temp-file company-distel zygospore xcscope ws-butler volatile-highlights undo-tree tabbar-ruler sws-mode sr-speedbar smartparens py-autopep8 magit jedi jdee jade-mode iedit highlight-current-line helm-swoop helm-projectile helm-gtags go-guru go-errcheck go-autocomplete ggtags function-args flycheck-tip exec-path-from-shell emacs-eclim elpy ein ecb duplicate-thing dtrt-indent company-jedi company-c-headers comment-dwim-2 clean-aindent-mode auto-complete-distel anzu))))
 
 ;; (custom-set-faces
 ;;  ;; custom-set-faces was added by Custom.
