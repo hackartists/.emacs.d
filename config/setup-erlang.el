9993399e513848a6a4d6951589e9ab38f930160d
Jongseok Choi <jseokchoi@smilegate.com>
2017-06-13 08:38:30 +0900

    Erlang flychecker was added.

--- config/setup-erlang.el
+++ config/setup-erlang.el
@@ -23,24 +23,29 @@
 ;;(require 'company-distel)
 (require 'company-distel)
 
-;;(flycheck-define-checker erlang-otp
-;;  "An Erlang syntax checker using the Erlang interpreter."
-;;  :command ("erlc" "-o" temporary-directory "-Wall"
-;;            "-I" "../include" "-I" "../../include"
-;;           "-I" "../../../include" source)
-;;  :error-patterns
-;;  ((warning line-start (file-name) ":" line ": Warning:" (message) line-end)
-;;   (error line-start (file-name) ":" line ": " (message) line-end)))
+(flycheck-define-checker erlang-otp
+ "An Erlang syntax checker using the Erlang interpreter."
+ :command ("erlc" "-Wall" "-pa" "../deps/lager/ebin"
+           "-I" "../include" "-I" "../../include"
+          "-I" "../../../include" source-original)
+ :error-patterns
+ (
+  (warning line-start (file-name) ":" line ": Warning:" (message) line-end)
+  (error line-start (file-name) ":" line ": " (message) line-end)
+  ;;(info line-start (file-name) ":" (message) line-end)
+  )
+ :modes (erlang-mode))
 
 (add-hook 'erlang-mode-hook
           (lambda ()
             (add-to-list 'company-backends 'company-distel)
-            (add-to-list 'flycheck-erlang-include-path "../include")
+            (local-set-key (kbd "RET") 'newline-and-indent)
+            ;;(add-to-list 'flycheck-erlang-include-path "../include")
             ;;(setq company-distel-popup-help t)
             ;;(setq company-distel-popup-height 30)
             ;;(setq inferior-erlang-machine-options '("-sname" "emacs@localhost"))
             (imenu-add-to-menubar "imenu")
-            ;;(flycheck-select-checker 'erlang-otp)
+            (flycheck-select-checker 'erlang-otp)
             (flycheck-mode)
             ))
 
