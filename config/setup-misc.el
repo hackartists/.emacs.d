a471099352bf4309ab19800217d70978d020ff0f
Jongseok Choi <js.choi.85@gmail.com>
2018-01-03 09:03:03 +0900

    misc

---
+++ config/setup-misc.el
@@ -0,0 +1,7 @@
+(defun my-command-error-function (data context caller)
+  "Ignore the buffer-read-only signal; pass the rest to the default handler."
+  (when (not (eq (car data) 'buffer-read-only))
+    (command-error-default-function data context caller)))
+
+(setq command-error-function #'my-command-error-function)
+(provide 'setup-misc)
