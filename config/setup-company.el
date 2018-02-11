a773feebb75d2996ac4896fbe142c2b10a1fe1bc
Jongseok Choi <js.choi.85@gmail.com>
2018-02-11 19:01:15 +0900

    In company-active-mode, return key will abort active mode and then insert newline.

--- config/setup-company.el
+++ config/setup-company.el
@@ -1,8 +1,14 @@
 (require 'company)
+(defun my-company-active-return ()
+  (interactive)
+  (company-abort)
+  (newline-and-indent)
+  )
+
 (eval-after-load 'company
   '(progn
      (define-key company-active-map (kbd "TAB") 'company-complete)
-     (define-key company-active-map (kbd "<return>") 'company-abort)
+     (define-key company-active-map (kbd "<return>") 'my-company-active-return)
      (define-key company-active-map [tab] 'company-complete)))
 ;; (add-hook 'after-init-hook 'global-company-mode)
 (global-company-mode t)
