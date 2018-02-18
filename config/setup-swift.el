c313b16afdf47ee5b90245b0b1df7ae46ee834a3
Jongseok Choi <js.choi.85@gmail.com>
2018-02-18 10:49:43 +0900

    swift

--- config/setup-swift.el
+++ config/setup-swift.el
@@ -3,6 +3,21 @@
             (push "~/.emacs.d/refs/company-sourcekit" load-path)
             (require 'company-sourcekit)
             (add-to-list 'company-backends 'company-sourcekit)
-            ))
+
+            (flycheck-define-checker swift-lint
+              "swift-lint will check errors and idiom of swift language."
+              :command ("swiftlint" "lint")
+              :error-patterns
+              (
+               (warning line-start (file-name) ":" line ": warning: " (message) line-end)
+               (error line-start (file-name) ":" line ": error: " (message) line-end)
+               ;;(info line-start (file-name) ":" (message) line-end)
+               )
+              :modes (swift-mode))
+
+            (flycheck-select-checker 'swift-lint)
+            (flycheck-mode)
+            )
+          )
 
 (provide 'setup-swift)
