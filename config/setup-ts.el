a1323dbd603d2f5d79190ab07794d6caa94cc729
Jongseok Choi <jseokchoi@smartm2m.co.kr>
2018-12-16 15:00:12 +0900

    typescript flycheck-mode

--- config/setup-ts.el
+++ config/setup-ts.el
@@ -1,6 +1,9 @@
 (add-hook 'typescript-mode-hook
           (lambda ()
             (setq typescript-indent-level 2)
+            (tide-setup)
+            (tide-mode)
+            (flycheck-mode)
             ))
 
 (provide 'setup-ts)
