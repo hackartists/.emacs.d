347532d9befb80fb578e767e345024f85208942b
Jongseok Choi <jseokchoi@smartm2m.co.kr>
2018-12-16 14:33:20 +0900

    typescript

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ cd jdee-server
 mvn -Dmaven.test.skip=true package
 
 # flymake-json mode
-npm install -g jsonlint indium tern
+npm install -g jsonlint indium tern eslint babel-eslint eslint-plugin-react eslint-plugin-node tslint tslint-eslint-rules tslint-config-prettier tide
 
 # rtags
 export CLANG=`xcrun -f clang++`
