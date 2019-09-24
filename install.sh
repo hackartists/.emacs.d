232c21f89a2ab5104843cb041c88dd05e570c18b
hackartist <hackartist@hackartists-MacBook-Pro.local>
2019-09-25 00:59:13 +0900

    golang install script

--- install.sh
+++ install.sh
@@ -55,6 +55,18 @@ brew cask install docker emacs jandi mactex postman robo-3t spectacle sublime-te
 go get golang.org/x/lint/golint
 go get -u golang.org/x/tools/...
 go get -u github.com/go-delve/delve/cmd/dlv
+go get -u github.com/kisielk/errcheck
+go get -u github.com/dougm/goflymake
+go get -u github.com/nsf/gocode
+go get -u github.com/rogpeppe/godef
+go get -u github.com/jstemmer/gotags
+go get -u github.com/motemen/gore
+go get -u github.com/k0kubun/pp
+go get -u github.com/mdempsky/unconvert
+go get -u github.com/dominikh/go-tools
+go get -u honnef.co/go/tools/cmd/megacheck
+go get -u github.com/fatih/gomodifytags
+go get -u github.com/lukehoban/go-outline
 
 # groovy setting
 cd /usr/local/opt/groovy/libexec
