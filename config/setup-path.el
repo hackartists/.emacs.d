61a83b07b8c90968b02201d1f172b2dfe50647f0
Jongseok Choi <hackartist@Jongseoks-MacBook-Pro.local>
2019-11-05 00:18:57 +0900

    go

--- config/setup-path.el
+++ config/setup-path.el
@@ -1,6 +1,11 @@
 
+(use-package exec-path-from-shell
+  :ensure t)
+
 (setq to-install
       '(exec-path-from-shell))
+
+(setq shell-file-name "/bin/zsh")
 
 (exec-path-from-shell-initialize)
 (exec-path-from-shell-copy-env "GOROOT")
