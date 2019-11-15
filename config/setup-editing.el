623ddbf000e860cd6cfa8b2d992b73e56f1d982a
Jongseok Choi <js.choi.85@gmail.com>
2019-11-15 10:12:43 +0900

    current line     # Please enter the commit message for your changes. Lines starting     # with '#' will be ignored, and an empty message aborts the commit.     #     # On branch master     # Your branch is up to date with 'origin/master'.     #     # Changes to be committed:     #	modified:   .spacemacs     #	modified:   config/setup-editing.el     #     # Untracked files:     #	org/     #

--- config/setup-editing.el
+++ config/setup-editing.el
@@ -1,32 +1,37 @@
 ;; GROUP: Editing -> Editing Basics
-(use-package treemacs
-  :ensure t
-  :bind (
-         :map treemacs-mode-map
-              ("RET" . treemacs-visit-node-in-most-recently-used-windows)
-         )
-  )
+(use-package 
+  treemacs 
+  :ensure t 
+  :bind (:map treemacs-mode-map
+              ("RET" . treemacs-visit-node-in-most-recently-used-windows)))
 
-(set-face-attribute 'hl-line nil :height (+ (face-attribute 'default :height) 10) :bold t :underline t :inherit nil :background nil)
+(set-face-attribute 'hl-line nil ;; :height (+ (face-attribute 'default :height) 30)
+                    :bold t 
+                    :underline t 
+                    :inherit nil 
+                    :background nil)
 
-(setq global-mark-ring-max 50000         ; increase mark ring to contains 5000 entries
-      mark-ring-max 50000                ; increase kill ring to contains 5000 entries
-      mode-require-final-newline t      ; add a newline to end of file
-      tab-width 4                       ; default to 4 visible spaces to display a tab
+(global-centered-cursor-mode 1)
+(add-hook 'emacs-lisp-mode-hook (lambda () 
+                                  (add-hook 'before-save-hook 'elisp-format-buffer)))
+
+(setq global-mark-ring-max 50000   ; increase mark ring to contains 5000 entries
+      mark-ring-max 50000          ; increase kill ring to contains 5000 entries
+      mode-require-final-newline t ; add a newline to end of file
+      tab-width 4                 ; default to 4 visible spaces to display a tab
       )
 
-(with-eval-after-load 'git-gutter+
-  (defun git-gutter+-remote-default-directory (dir file)
-    (let* ((vec (tramp-dissect-file-name file))
-           (method (tramp-file-name-method vec))
-           (user (tramp-file-name-user vec))
-           (domain (tramp-file-name-domain vec))
-           (host (tramp-file-name-host vec))
-           (port (tramp-file-name-port vec)))
-      (tramp-make-tramp-file-name method user domain host port dir)))
-
-  (defun git-gutter+-remote-file-path (dir file)
-    (let ((file (tramp-file-name-localname (tramp-dissect-file-name file))))
+(with-eval-after-load 'git-gutter+ 
+  (defun git-gutter+-remote-default-directory (dir file) 
+    (let* ((vec (tramp-dissect-file-name file)) 
+           (method (tramp-file-name-method vec)) 
+           (user (tramp-file-name-user vec)) 
+           (domain (tramp-file-name-domain vec)) 
+           (host (tramp-file-name-host vec)) 
+           (port (tramp-file-name-port vec))) 
+      (tramp-make-tramp-file-name method user domain host port dir))) 
+  (defun git-gutter+-remote-file-path (dir file) 
+    (let ((file (tramp-file-name-localname (tramp-dissect-file-name file)))) 
       (replace-regexp-in-string (concat "\\`" dir) "" file))))
 
 (global-linum-mode)
@@ -40,7 +45,7 @@
 
 (global-set-key (kbd "M-RET") 'yas-insert-snippet)
 (setq make-backup-files nil)
-(add-hook 'sh-mode-hook (lambda ()
+(add-hook 'sh-mode-hook (lambda () 
                           (setq tab-width 4)))
 
 (set-terminal-coding-system 'utf-8)
@@ -53,43 +58,36 @@
 (global-set-key (kbd "RET") 'newline-and-indent)
 
 ;; GROUP: Editing -> Killing
-(setq kill-ring-max 50000 ; increase kill-ring capacity
-      kill-whole-line t  ; if NIL, kill whole line and move the next line up
+(setq kill-ring-max 50000    ; increase kill-ring capacity
+      kill-whole-line t      ; if NIL, kill whole line and move the next line up
       )
 
 ;; show whitespace in diff-mode
-(add-hook 'diff-mode-hook (lambda ()
-                            (setq-local whitespace-style
-                                        '(face
-                                          tabs
-                                          tab-mark
-                                          spaces
-                                          space-mark
-                                          trailing
-                                          indentation::space
-                                          indentation::tab
-                                          newline
-                                          newline-mark))
+(add-hook 'diff-mode-hook (lambda () 
+                            (setq-local whitespace-style '(face tabs tab-mark spaces space-mark
+                                                                trailing indentation: 
+                                                                :space indentation: 
+                                                                :tab newline newline-mark)) 
                             (whitespace-mode 1)))
 
 ;; Jump to end of snippet definition
 (define-key yas-keymap (kbd "<return>") 'yas/exit-all-snippets)
 
 ;; Inter-field navigation
-(defun yas/goto-end-of-active-field ()
-  (interactive)
-  (let* ((snippet (car (yas--snippets-at-point)))
-         (position (yas--field-end (yas--snippet-active-field snippet))))
-    (if (= (point) position)
-        (move-end-of-line 1)
+(defun yas/goto-end-of-active-field () 
+  (interactive) 
+  (let* ((snippet (car (yas--snippets-at-point))) 
+         (position (yas--field-end (yas--snippet-active-field snippet)))) 
+    (if (= (point) position) 
+        (move-end-of-line 1) 
       (goto-char position))))
 
-(defun yas/goto-start-of-active-field ()
-  (interactive)
-  (let* ((snippet (car (yas--snippets-at-point)))
-         (position (yas--field-start (yas--snippet-active-field snippet))))
-    (if (= (point) position)
-        (move-beginning-of-line 1)
+(defun yas/goto-start-of-active-field () 
+  (interactive) 
+  (let* ((snippet (car (yas--snippets-at-point))) 
+         (position (yas--field-start (yas--snippet-active-field snippet)))) 
+    (if (= (point) position) 
+        (move-beginning-of-line 1) 
       (goto-char position))))
 
 (define-key yas-keymap (kbd "C-e") 'yas/goto-end-of-active-field)
@@ -106,7 +104,8 @@
 ;; Wrap around region
 (setq yas-wrap-around-region t)
 
-(add-hook 'term-mode-hook (lambda() (setq yas-dont-activate t)))
+(add-hook 'term-mode-hook (lambda() 
+                            (setq yas-dont-activate t)))
 
 ;; PACKAGE: anzu
 ;; GROUP: Editing -> Matching -> Isearch -> Anzu
@@ -259,7 +258,7 @@
 ;;(global-set-key (kbd "M-o") 'prelude-smart-open-line)
 ;;(global-set-key (kbd "M-o") 'open-line)
 
-(add-hook 'flycheck-mode-hook (lambda()
+(add-hook 'flycheck-mode-hook (lambda() 
                                 (flycheck-list-errors)))
 
 (provide 'setup-editing)
