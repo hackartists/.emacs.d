a0dba0f494cf24774682784d35e4e13e50121ff0
hackartist <hackartists@gmail.com>
2021-03-20 20:55:21 +0900

    disable yas expand key

--- apps/ide/deps.el
+++ apps/ide/deps.el
@@ -17,7 +17,7 @@
                          auto-completion-minimum-prefix-length 1
                          auto-completion-idle-delay 0.2
                          auto-completion-private-snippets-directory nil
-                         auto-completion-enable-snippets-in-popup nil
+                         auto-completion-enable-snippets-in-popup t
                          auto-completion-enable-help-tooltip nil
                          auto-completion-use-company-box nil
                          auto-completion-enable-sort-by-usage nil)
