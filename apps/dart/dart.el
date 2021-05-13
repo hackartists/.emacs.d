7b0b2dece6ff2ff27d6a6913cdcb817b70c1aae0
hackartist <js.choi.85@gmail.com>
2021-05-14 00:07:22 +0900

    dart lsp

--- apps/dart/dart.el
+++ apps/dart/dart.el
@@ -1,9 +1,11 @@
 (setq hackartist-dart-layers
       '(
-        (dart :variables dart-backend 'lsp lsp-dart-sdk-dir "/opt/flutter/bin/cache/dart-sdk" dart-lsp-enable-on-type-formatting t dart-server-format-on-save t)
+        (dart :variables dart-backend 'lsp lsp-dart-sdk-dir "/opt/flutter/bin/cache/dart-sdk" lsp-enable-on-type-formatting t)
         ))
 
-(defun hackartist/dart/init ())
+(defun hackartist/dart/init ()
+  (add-hook 'dart-mode-hook 'hackartist/dart/before-save-hook))
+
 (defun hackartist/dart/bindings ()
   "key bindings in dart mode"
   (spacemacs/declare-prefix-for-mode 'dart-mode "f" "flutter")
@@ -11,3 +13,8 @@
     "RET" 'lsp-dart-dap--flutter-hot-reload
     "SPC" 'flutter-run-or-hot-reload)
   )
+
+(defun hackartist/dart/before-save-hook ()
+  (when (derived-mode-p 'dart-mode)
+    (lsp-format-buffer)
+    (flutter-run-or-hot-reload)))
