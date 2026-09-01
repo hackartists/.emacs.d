4fb702aa482fde767844eed88d3d90bd4ea3a7cf
hackartists <hackartists@gmail.com>
2026-09-01 15:01:27 +0900

    feat(docker): bind L key to docker-container-logs in motion mode

--- apps/docker/docker.el
+++ apps/docker/docker.el
@@ -29,4 +29,5 @@
 
 (defun hackartist/docker/bindings ()
   (with-eval-after-load 'docker-container
+    (evil-define-key 'normal docker-motion-mode-map (kbd "L") 'docker-container-logs)
     (evil-define-key 'normal docker-container-mode-map (kbd "L") 'docker-container-logs)))
